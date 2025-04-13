9d984c97b03efbfb31fb21b25244ac6b8bdf78ae
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2025-04-12 20:36:50 -0500

    mercado

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import Vue from 'vue'
+import ExampleComponent from './components/ExampleComponent.vue'
+
+new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent
+    }
+});
